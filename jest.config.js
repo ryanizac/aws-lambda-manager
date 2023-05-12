a3f4dba745d06b7da8c65186a6b529026b08aa74
Ryan Izac <ryanizac.infor@gmail.com>
2023-05-11 21:05:42 -0300

    chore: add typescript and jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @satisfies {import("jest").Config} */
+module.exports = {
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  testMatch: ["**/*.spec.ts", "**/*.test.ts"],
+};
